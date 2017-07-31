d25716fb0116a1acfce2ef849e974a31236ad9cd
Matt Aitken <matt.aitken@gmail.com>
2017-07-31 16:33:18 +1000

    paint divs

--- pixart.js
+++ pixart.js
@@ -13,9 +13,14 @@ $(document).ready(function () {
   console.log('DOM ready')
 })
 
+var paint = function(event) {
+  event.target.style.backgroundColor = currentColor
+}
+
 var background = document.querySelector('#body')
   for (var i = 0; i < 20; i++) {
     var div = document.createElement('div')
     div.className += 'square'
     background.appendChild(div)
+    div.addEventListener('click', paint)
   }
