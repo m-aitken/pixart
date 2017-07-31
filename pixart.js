af9a8b97c8aec8b2f24e25a4090edf1ace77af24
Matt Aitken <matt.aitken@gmail.com>
2017-07-31 16:30:59 +1000

    add divs

--- pixart.js
+++ pixart.js
@@ -2,8 +2,20 @@ var brush = document.querySelector('.brush')
 var currentColor = document.getElementById('color-field').value
 
 controlForm = document.querySelector('form')
+
 controlForm.addEventListener('submit', function(event) {
   event.preventDefault()
   currentColor = event.target.querySelector('input').value
   brush.style.backgroundColor = currentColor
 })
+
+$(document).ready(function () {
+  console.log('DOM ready')
+})
+
+var background = document.querySelector('#body')
+  for (var i = 0; i < 20; i++) {
+    var div = document.createElement('div')
+    div.className += 'square'
+    background.appendChild(div)
+  }
